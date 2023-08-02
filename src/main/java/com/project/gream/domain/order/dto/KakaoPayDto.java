71b110312cf0c454cf1ddc7fdb0f83cee7d7448b
mynameiskuun <mynameiskuun@gmail.com>
2023-08-02 15:13:16 +0900

    Gream v2.2

--- src/main/java/com/project/gream/domain/order/dto/KakaoPayDto.java
+++ src/main/java/com/project/gream/domain/order/dto/KakaoPayDto.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Map;
 
 @NoArgsConstructor
 @Getter
@@ -15,7 +16,8 @@ public class KakaoPayDto {
     private String tel;
     private String email;
     private int usePoint;
-    private int couponDiscountAmount;
+    private Map<Long, Long> couponDiscountMap;
+    private Long couponDiscountAmount;
     private int pointRewardAmount;
     private int size;
     private String itemName;
--- src/main/java/com/project/gream/domain/order/service/OrderService.java
+++ src/main/java/com/project/gream/domain/order/service/OrderService.java
@@ -15,10 +15,14 @@ public interface OrderService {
     KakaoPayRequestVO kakaoPayReady(KakaoPayDto request);
     KakaoPayApprovedResultVO payApprove(String tid, String pgToken, KakaoPayDto request);
     void updateDataBaseAfterPayment(KakaoPayDto kakaoPayDto, @LoginMember MemberDto memberDto);
-    void sendPaymentReceiptEmail(KakaoPayApprovedResultVO result, @LoginMember MemberDto memberDto, int totalDiscountAmount) throws Exception;
+    void sendPaymentReceiptEmail(KakaoPayApprovedResultVO result, @LoginMember MemberDto memberDto, Long totalDiscountAmount) throws Exception;
     List<OrderItemDto> findOrderItemForMypage(String memberId);
     Page<OrderItemDto> findAllOrderItem(String memberId, Pageable pageable);
     List<OrderItemDto> sortByOrderState(String sortBy, MemberDto memberDto);
 
     List<OrderHistoryDto> findTop5OrderByCreatedTimeDesc();
+
+    Page<OrderResponseDto> getAllOrderItemsForAdmin(Pageable pageable);
+
+    OrderResponseDto changeOrderStatus(OrderRequestDto request);
 }
